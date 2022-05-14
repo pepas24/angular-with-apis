94c19781126d3c30730493e4339131518b468c44
Sebastian Gutierrez <sebastian@rocketfy.co>
2022-05-14 12:29:43 -0500

    add pokemon mock example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { pokemonMock } from './services/pokemon.mock';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-with-apis';
+  pokemon = pokemonMock;
+
+  pokemonTypeColorMapping: Record<string, string> = {
+    normal: 'gray',
+    hierba: 'green',
+  }
+  pokemonTypeColor = '#4eceb1';
 }
---
+++ src/app/services/pokemon.mock.ts
@@ -0,0 +1,19 @@
+export const pokemonMock = {
+  name: 'Ditto',
+  images: {
+    'front': 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png',
+    'back': 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/132.png',
+  },
+  type: 'hierba',
+  weigth: 120,
+  habilities: [
+    {
+      name: 'correr',
+      value: 5,
+    },
+    {
+      name: 'salto',
+      value: 8,
+    }
+  ]
+};
